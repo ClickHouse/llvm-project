b9184ed2ecbd606ed0395ce4002eec3ec3ad4cb5
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-03-06 21:02:12 +0100

    Missing patch for libunwind

--- libunwind/src/UnwindCursor.hpp
+++ libunwind/src/UnwindCursor.hpp
@@ -2696,15 +2696,6 @@ void UnwindCursor<A, R>::setInfoBasedOnIPRegister(bool isReturnAddress) {
     --pc;
 #endif
 
-#if !(defined(_LIBUNWIND_SUPPORT_SEH_UNWIND) && defined(_WIN32)) &&            \
-    !defined(_LIBUNWIND_SUPPORT_TBTAB_UNWIND)
-  // In case of this is frame of signal handler, the IP saved in the signal
-  // handler points to first non-executed instruction, while FDE/CIE expects IP
-  // to be after the first non-executed instruction.
-  if (_isSignalFrame)
-    ++pc;
-#endif
-
   // Ask address space object to find unwind sections for this pc.
   UnwindInfoSections sects;
   if (_addressSpace.findUnwindSections(pc, sects)) {
