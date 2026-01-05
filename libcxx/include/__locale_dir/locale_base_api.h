0eb47540816a37324403ed3fee64977c1fe04496
Josh Carp <jm.carp@gmail.com>
2026-01-05 16:17:21 +0000

    Add illumos support to libc++ and LLVM.
    libc++ changes:
    - Add __sun to pthread platforms and rune table providers.
    - Add illumos ctype mask definitions with non-overlapping __regex_word.
    - Use musl locale shim for missing *_l functions.
    - Skip overloads already defined in illumos stdlib.
    - Fix conflicting __sentinel, _E1-_E24 macros.
    
    LLVM changes:
    - Fix conflicting FS, CS/CE macros.
    - Use std::fabs instead of std::abs for float in SampleProfileProbe.
    
    Part of #23777.

--- libcxx/include/__locale_dir/locale_base_api.h
+++ libcxx/include/__locale_dir/locale_base_api.h
@@ -133,7 +133,7 @@
 #      include <__locale_dir/locale_base_api/android.h>
 #    elif defined(__OpenBSD__)
 #      include <__locale_dir/locale_base_api/openbsd.h>
-#    elif defined(__wasi__) || _LIBCPP_HAS_MUSL_LIBC
+#    elif defined(__wasi__) || _LIBCPP_HAS_MUSL_LIBC || defined(__sun)
 #      include <__locale_dir/locale_base_api/musl.h>
 #    endif
 
